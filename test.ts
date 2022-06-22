e4f638bb00e5e4bc3eb4f0580e19d23c83cae3e7
jaromir <jaromirblu@gmail.com>
2022-06-22 17:10:39 +0200

    updated test.ts

--- test.ts
+++ test.ts
@@ -90,24 +90,85 @@ Deno.test("test create_db_client()", async () => {
     await client.close();
 });
 
-// TODO TEST API
+
+// TEST API
+/**
+ * create_testuser() creates new user for testing and returns reponse from server
+ * 
+ * @returns response
+ */
+async function create_testuser(): Promise<Response> {
+    const username = "testuser";
+    const password = "password";
+
+    // create user
+    const response = await fetch("http://localhost:8000/signup", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username: username, password: password})
+    });
+
+    return response;
+}
+
+
+/**
+ * delete_testuser() takes token and deletes user for testing
+ * 
+ * @param token 
+ * @returns response
+ */
+async function delete_testuser(token: string): Promise<Response> {
+
+    // delete user with token
+    const response = await fetch("http://localhost:8000/deluser", {
+        method: "DELETE",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({token: token}) // use token
+    });
+
+    return response;
+}
+
+
+/**
+ * login_testuser() logs testuser in to get reponse containing token (jwt)
+ * 
+ * @returns response
+ */
+async function login_testuser(): Promise<Response> {
+    const username = "testuser";
+    const password = "password";
+
+    // login user to get response with token
+    const response = await fetch("http://localhost:8000/login", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username: username, password: password})
+    });
+
+    return response;
+}
+
+
 /**
  * test /signup
  */
 Deno.test({
     name: "test /signup (creating testuser)",
     async fn () { 
-        const username = "testuser";
-        const password = "password";
-
-        const response = await fetch("http://localhost:8000/signup", {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({username: username, password: password})
-        });
+    
+        // create testuser
+        const response = await create_testuser();
 
         assertEquals(response.status, 200); // check if ok
 
@@ -115,7 +176,7 @@ Deno.test({
 
         assertEquals(token.split(".").length, 3); // check if token has valid syntax
     },
-    // deactivate ressource checking (else error because leaking async ops)
+    // deactivate resource checking (else error because leaking async ops)
     sanitizeResources: false,
     sanitizeOps: false
 });
@@ -127,17 +188,7 @@ Deno.test({
 Deno.test({
     name: "test /login (logging in as testuser)",
     async fn () { 
-        const username = "testuser";
-        const password = "password";
-
-        const response = await fetch("http://localhost:8000/login", {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({username: username, password: password})
-        });
+        const response = await login_testuser(); // login
 
         assertEquals(response.status, 200); // check if ok
 
@@ -145,7 +196,7 @@ Deno.test({
 
         assertEquals(token.split(".").length, 3); // check if token has valid syntax
     },
-    // deactivate ressource checking (else error because leaking async ops)
+    // deactivate resource checking (else error because leaking async ops)
     sanitizeResources: false,
     sanitizeOps: false
 });
@@ -157,49 +208,168 @@ Deno.test({
  Deno.test({
     name: "test /deluser (delete testuser)",
     async fn () { 
-        const username = "testuser";
-        const password = "password";
 
         // login to get token (should work)
-        const response = await fetch("http://localhost:8000/login", {
+        const response = await login_testuser();
+        assertEquals(response.status, 200); // check if ok
+        const token = await response.text(); // get jwt
+
+        // delete user with token
+        const response_del = await delete_testuser(token);
+        assertEquals(response_del.status, 200); // check if ok
+
+        // login again (should not work, user was deleted)
+        const response_retry = await login_testuser();
+        assertEquals(response_retry.status, 403); // check if Forbidden
+    },
+    // deactivate resource checking (else error because leaking async ops)
+    sanitizeResources: false,
+    sanitizeOps: false
+});
+
+/**
+ * test /addpayments
+ */
+Deno.test({
+    name: "test /addpayments",
+    async fn () { 
+        // create testuser
+        const response = await create_testuser();
+        assertEquals(response.status, 200); // check if ok
+        const token = await response.text(); // get jwt
+
+        // test data
+        const payments = [
+            {date: "2020-10-10", amount: 10, time: "10:30:00", category: "business", text: "some text about payment"},
+            {date: "2022-01-01", amount: 150, time: "15:00:00", category: "business", text: "some more text about payment"}
+        ];
+
+        // send payments to server (should work)
+        const response_add = await fetch("http://localhost:8000/addpayments", {
             method: "POST",
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({username: username, password: password})
+            body: JSON.stringify({payments: payments, token: token})
         });
 
-        assertEquals(response.status, 200); // check if ok
+        assertEquals(response_add.status, 200); // should be ok
+        console.log(await response_add.text());
+    },
+    // deactivate resource checking (else error because leaking async ops)
+    sanitizeResources: false,
+    sanitizeOps: false
+});
 
-        const token = await response.text(); // get jwt
+
+/**
+ * test /payments
+ */
+Deno.test({
+    name: "test /payments",
+    async fn () { 
+        // test data for comparison
+        const compare_payments = [
+            {date: "2020-10-10", amount: 10, time: "10:30:00", category: "business", text: "some text about payment"},
+            {date: "2022-01-01", amount: 150, time: "15:00:00", category: "business", text: "some more text about payment"}
+        ];
+
+        const response_login = await login_testuser(); // login to get token
+        const token = await response_login.text(); // get token
+
+        // get payments from server (should work)
+        const response = await fetch("http://localhost:8000/payments", {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({token: token})
+        });
+
+        assertEquals(response.status, 200); // should be ok
+        const payments = JSON.parse(await response.text()); // get payments from server
+
+        // check if payments are same as compare_payments (ignoring date, as format is different)
+        for (let i = 0; i < payments.length; i++) {
+            const payment = payments[i];
+            const compare_payment = compare_payments[i];
+
+            assertEquals(compare_payment.amount, payment.payment_amount); // check amount
+            assertEquals(compare_payment.category, payment.payment_category); // check category
+            assertEquals(compare_payment.text, payment.payment_text); // check text
+            assertEquals(compare_payment.time, payment.payment_time); // check time
+        }
+
+    },
+    // deactivate resource checking (else error because leaking async ops)
+    sanitizeResources: false,
+    sanitizeOps: false
+});
 
 
-        // delete user with token
-        const response_del = await fetch("http://localhost:8000/deluser", {
+/**
+ * test /delpayments
+ */
+Deno.test({
+    name: "test /delpayments",
+    async fn () { 
+        const response_login = await login_testuser(); // login
+        const token = await response_login.text(); // get token
+
+        // get payments from server (should work)
+        const response = await fetch("http://localhost:8000/payments", {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({token: token})
+        });
+
+        assertEquals(response.status, 200); // should be ok
+        const payments = JSON.parse(await response.text()); // get payments from server
+
+        // get ids of all payments to delete them later
+        let payment_ids: number[] = [];
+        for (let i = 0; i < payments.length; i++) {
+            const id = payments[i].id; // get id
+            payment_ids.push(id); 
+        }
+
+        // delete all payments from server (should work)
+        const response_del = await fetch("http://localhost:8000/delpayments", {
             method: "DELETE",
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({token: token}) // use token
+            body: JSON.stringify({token: token, payment_ids: payment_ids})
         });
 
-        assertEquals(response_del.status, 200); // check if ok
+        assertEquals(response_del.status, 200); // should be ok
 
-        // login again (should not work, user was deleted)
-        const response_retry = await fetch("http://localhost:8000/login", {
+
+        // check if all payments are deleted
+        const response_payment = await fetch("http://localhost:8000/payments", {
             method: "POST",
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({username: username, password: password})
+            body: JSON.stringify({token: token})
         });
 
-        assertEquals(response_retry.status, 403); // check if Forbidden
+        assertEquals(response_payment.status, 200); // should be ok
+        assertEquals(JSON.parse(await response_payment.text()), []); // get payments from server, should be empty array
+
+        // delete testuser
+        const response_del_user = await delete_testuser(token);
+        assertEquals(response_del_user.status, 200); // should be ok
+
     },
-    // deactivate ressource checking (else error because leaking async ops)
+    // deactivate resource checking (else error because leaking async ops)
     sanitizeResources: false,
     sanitizeOps: false
 });
